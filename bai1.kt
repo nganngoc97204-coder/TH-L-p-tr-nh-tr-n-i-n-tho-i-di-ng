b9c5f47d53d3c59f98c8e302ca75ef9780000c24
PC-25\PC-25 <nganngoc97204@gmail.com>
2026-08-25 14:36:06 +0700

    Thêm bài 1

---
+++ bai1.kt
@@ -0,0 +1,25 @@
+fun main() {
+    val hoten = "Nguyen Vu Hoang Ngan"
+    val msv = "2415141122112"
+    val lsh = "24SK1"
+    val lhp = "126TLTTD02"
+    val diem = "A"
+    println("Xin chao ${hoten}")
+    println("Ma sinh vien ${msv}")
+    println("Lop sinh hoat ${lsh}")
+    println("Lop hoc phan ${lhp}")
+    println("Diem ${diem}")
+    
+    val toan = 10
+    val van = 10
+    val anh = 9
+    val diemTB = (toan + van + anh)/3
+    if (diemTB>5)
+    {
+        println ("Diem trung binh lon hon nam")
+    }
+    else 
+    {
+        println ("Diem trung binh be hon nam")
+    }
+}
